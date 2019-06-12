d61078c0f180e9832a0f54788d18c1574d9ee7c3
AidaRos95 <aidajros@gmail.com>
2019-06-12 18:22:02 +0200

    Renders event details to page

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -4,11 +4,23 @@ import { Link } from 'react-router-dom'
 class EventDetails extends React.Component {
 
   render() {
+    const event = this.props.event
+
+    function showEvent(event) {
+      if (!event) {
+        return <p>Loading event details...</p>
+      }
+      return <div>
+        <h1>Event details</h1>
+        <p>Name: {event.name}</p>
+        <p>Date: {event.start} - {event.end}</p>
+        <p>Description: {event.description}</p>
+      </div>
+    }
 
     return (
       <main>
-        <h1>Event details</h1>
-        {this.props.events}
+        {showEvent(event)}
       </main>
     );
   }
