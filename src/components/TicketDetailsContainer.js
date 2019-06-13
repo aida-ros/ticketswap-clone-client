249c4486e088198639c9b8c7f7ecef7d665cb78c
AidaRos95 <aidajros@gmail.com>
2019-06-13 14:37:58 +0200

    Single ticket details now fetched in Redux state

--- src/components/TicketDetailsContainer.js
+++ src/components/TicketDetailsContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import TicketDetails from './TicketDetails.js'
 import { connect } from 'react-redux'
-import { getEvent, getTickets } from '../actions'
+import { getTicket } from '../actions'
 
 class TicketDetailsContainer extends React.Component {
+  componentDidMount = () => {
+    const id = this.props.match.params.id
+    this.props.getTicket(id)
+  }
 
   render() {
     return (
@@ -12,4 +16,12 @@ class TicketDetailsContainer extends React.Component {
   }
 }
 
-export default TicketDetailsContainer
+const mapStateToProps = state => ({
+  ticket: state.ticket
+})
+
+const mapDispatchToProps = {
+  getTicket
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TicketDetailsContainer)
